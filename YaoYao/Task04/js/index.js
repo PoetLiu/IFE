679c76447ddc1bf2bb29ca1d7df3ad8309947eb6
Albert Liu <457566268@qq.com>
2017-11-21 15:07:02 +0800

    Mod: Add chess Move control.

--- YaoYao/Task04/js/index.js
+++ YaoYao/Task04/js/index.js
@@ -1,7 +1,7 @@
 var chessBoard = document.getElementById('chess-board');
 var chessBoardData = [];
 var chess;
-const ROW = 10, COL = 10;
+const ROW = 10, COL = 10, UP = 0, RIGHT = 1, DOWN = 2, LEFT = 3;
 
 function onChessBoardClick(e) {
     console.log(chessBoardData.indexOf(e.target));
@@ -28,7 +28,7 @@ function loadChessBoard() {
     chessBoardData = Array.from(chessBoard.children);
 
     // add random chess.
-    chess = {id:randomChessIdx(), dom:newChess()};
+    chess = {id: randomChessIdx(), dom: newChess()};
     chessBoardData[chess.id].appendChild(chess.dom);
 }
 
@@ -46,9 +46,68 @@ function newChess() {
     return chess;
 }
 
+function onKeyDown(e) {
+    var k = e.key;
+    switch (k) {
+        case 'w':
+            moveChess(UP);
+            break;
+        case 's':
+            moveChess(DOWN);
+            break;
+        case 'a':
+            moveChess(LEFT);
+            break;
+        case 'd':
+            moveChess(RIGHT);
+            break;
+    }
+}
+
+function chessWillHitBorder(i, dir) {
+    var r = Math.floor(i / COL);
+    var c = i % COL;
+    return ((r === 0 && dir === UP) ||
+        (r === ROW - 1 && dir === DOWN) ||
+        (c === 0 && dir === LEFT) ||
+        (c === COL - 1 && dir === RIGHT));
+}
+
+function chessWithinBorder(i) {
+    return i >= 0 && i < COL * ROW;
+}
+
+function moveChess(dir) {
+    var idx = chess.id;
+
+    if (chessWillHitBorder(idx, dir)) {
+        return;
+    }
+
+    switch (dir) {
+        case UP:
+            idx = chess.id - (COL);
+            break;
+        case RIGHT:
+            idx = chess.id + 1;
+            break;
+        case DOWN:
+            idx = chess.id + (COL);
+            break;
+        case LEFT:
+            idx = chess.id - 1;
+            break;
+    }
+
+    chessBoardData[chess.id].removeChild(chess.dom);
+    chess.id = idx;
+    chessBoardData[chess.id].appendChild(chess.dom);
+}
+
 function init() {
     loadChessBoard();
     chessBoard.addEventListener('click', onChessBoardClick);
+    document.addEventListener('keydown', onKeyDown);
 }
 
 init();
