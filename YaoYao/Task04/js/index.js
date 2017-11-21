f70030ddc585b745a95d29fd30095fd544352e9e
Albert Liu <457566268@qq.com>
2017-11-21 14:20:14 +0800

    Mod: Add chess.

--- YaoYao/Task04/js/index.js
+++ YaoYao/Task04/js/index.js
@@ -3,21 +3,50 @@ var chessBoardData = [];
 const ROW = 10, COL = 10;
 
 function onChessBoardClick(e) {
-    console.log(e.target);
+    console.log(chessBoardData.indexOf(e.target));
+}
+
+function newChessCell() {
+    var cell = document.createElement('div');
+    cell.className += 'cell';
+    return cell;
+}
+
+function random(min, max) {
+    return Math.floor(Math.random() * max) - min;
+}
+
+function randomChessIdx() {
+    return random(0, ROW * COL);
 }
 
 function loadChessBoard() {
-   for (var i = 0; i < ROW*COL; i++) {
-       var cell = document.createElement('div');
-        cell.className  += 'cell';
-        chessBoardData.push({dom:cell, row:i%COL, col:i%ROW});
-        chessBoard.appendChild(cell);
-   }
+    for (var i = 0; i < ROW * COL; i++) {
+        chessBoard.appendChild(newChessCell());
+    }
+    chessBoardData = Array.from(chessBoard.children);
+
+    // add random chess.
+    chessBoardData[randomChessIdx()].appendChild(newChess());
+}
+
+function newChess() {
+    var chess = document.createElement('div');
+    chess.className += 'chess';
+
+    var div = document.createElement('div');
+    div.className += 'chess-head';
+    chess.appendChild(div);
+
+    div = document.createElement('div');
+    div.className += 'chess-body';
+    chess.appendChild(div);
+    return chess;
 }
 
 function init() {
     loadChessBoard();
-   chessBoard.addEventListener('click', onChessBoardClick);
+    chessBoard.addEventListener('click', onChessBoardClick);
 }
 
 init();
