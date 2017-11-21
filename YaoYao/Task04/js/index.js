23fc514f519cc3a0038e554f2b6d6930e83968e5
Albert Liu <457566268@qq.com>
2017-11-21 13:50:00 +0800

    Mod: Add chess cells.

--- YaoYao/Task04/js/index.js
+++ YaoYao/Task04/js/index.js
@@ -1,5 +1,23 @@
-function init() {
+var chessBoard = document.getElementById('chess-board');
+var chessBoardData = [];
+const ROW = 10, COL = 10;
 
+function onChessBoardClick(e) {
+    console.log(e.target);
+}
+
+function loadChessBoard() {
+   for (var i = 0; i < ROW*COL; i++) {
+       var cell = document.createElement('div');
+        cell.className  += 'cell';
+        chessBoardData.push({dom:cell, row:i%COL, col:i%ROW});
+        chessBoard.appendChild(cell);
+   }
+}
+
+function init() {
+    loadChessBoard();
+   chessBoard.addEventListener('click', onChessBoardClick);
 }
 
 init();
